1ad453122307dc418a371cee41578535d0221a10
Marcio Andre Mendes <marcioandre.ti2@gmail.com>
2024-12-16 20:39:15 -0300

    Desenvolvimento do componente de botão

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from "react-native"
+import { View } from "react-native"
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 import { Welcome } from "@/components/welcome"
 
 export default function Index() {
     return (
-        <View style={{ flex: 1, padding: 40, gap: 40 }}>
+        <View style={{ flex: 1, padding: 40, gap: 40 }}>                        
             <Welcome />
             <Steps />
+            <Button isLoading={false}>
+                <Button.Title>Começar</Button.Title>                
+            </Button>            
         </View>
     )
 }

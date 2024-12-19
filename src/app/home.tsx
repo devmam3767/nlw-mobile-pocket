0af8631800897b8c39330a87a2d1b9111ebe8d95
Marcio Andre Mendes <marcioandre.ti2@gmail.com>
2024-12-19 19:12:22 -0300

    Estilização dos components coategories e cotegory

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -6,11 +6,13 @@ import { Categories, CategoriesProps } from "@/components/categories"
 export default function Home () {
 
     const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
 
     async function fetchCategories() {
         try {
             const { data } = await api.get("/categories")
             setCategories(data)
+            setCategory(data[0].id)
         } catch (error) {
             console.log(error)
             Alert.alert("Categorias", "Não foi possível carregar as categorias.")
@@ -21,11 +23,9 @@ export default function Home () {
         fetchCategories()
     }, [])
 
-
-
     return <View style={{ flex: 1 }}>
                 
-        <Categories data={categories} />
+        <Categories data={categories} onSelect={setCategory} selected={category} />
         
     </View>
     
--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,5 +1,6 @@
 import { Text, Pressable, PressableProps } from "react-native"
 import { s } from "./styles"
+import { colors } from "@/styles/theme"
 import { categoriesIcons } from "@/utils/categories-icons"
 
 type Props = PressableProps & {
@@ -12,9 +13,9 @@ export function Category ({ name, iconId, isSelected = false, ...rest }: Props) 
     const Icon = categoriesIcons[iconId]
     
     return (
-        <Pressable style={[s.container]}>
-            <Icon size={16} />
-            <Text style={[s.name]}>{name}</Text>
+        <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest}>
+            <Icon size={16} color={colors.gray[isSelected ? 100 : 400]} />
+            <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
         </Pressable>
     )
 }
